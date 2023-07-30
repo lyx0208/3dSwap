fd70bea03b8be808c6f119d2eb142b09026bb7f2
Yixuan Li <65213004+lyx0208@users.noreply.github.com>
2023-07-30 16:42:51 +0800

    Add files via upload

--- configs/data_configs.py
+++ configs/data_configs.py
@@ -4,9 +4,9 @@ from configs import transforms_config
 DATASETS = {
 	'ffhq_encode': {
 		'transforms': transforms_config.EncodeTransforms,
-		'train_source_root': '/2t/datasets/EG3D/final_crops',
-		'train_target_root': '/2t/datasets/EG3D/final_crops',
-		'test_source_root': '/2t/datasets/EG3D_CelebA/final_crops',
-		'test_target_root':'/2t/datasets/EG3D_CelebA/final_crops',
+		'train_source_root': 'datasets/EG3D_FFHQ/final_crops',
+		'train_target_root': 'datasets/EG3D/final_crops',
+		'test_source_root': 'datasets/EG3D_CelebA/final_crops',
+		'test_target_root':'datasets/EG3D_CelebA/final_crops',
 	}
 }
